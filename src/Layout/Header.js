5a2935df76d63656206205bee14658cb64997642
Tyler Hoffman <tjhoffmn@gmail.com>
2023-02-05 21:18:49 -0800

    style update, more mobile friendly

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <div className="left-bar">
-      <Link to="/">
+      <Link to="/" id="tw-logo">
         <div className="tw-logo">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/292px-Twitter-logo.svg.png" width="30px" alt="Tweeter" />
         </div>
