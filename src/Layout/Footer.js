e52d7253cd0e4d4c49a6ddef860d0ab33c031fce
Tyler Hoffman <tjhoffmn@gmail.com>
2023-02-05 20:51:29 -0800

    mobile friendly

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -11,12 +11,12 @@ const Footer = () => {
   return !token && (
     <>
     <div className="tw-footer">
-      <div className="w-25"></div>
-      <div className="w-50">
+      <div className="w-25" id="tw-footer-left"></div>
+      <div className="w-50" id="tw-footer-mid">
         <h4 className="mb-0">Don’t miss what’s happening</h4>
         People on Twitter are the first to know.
       </div>
-      <div className="w-25 mt-2">
+      <div className="w-25 mt-2" id="tw-footer-right">
         <button className="login-btn me-2" onClick={() => setSignIn(!signIn)}>Log in</button>
         <button className="signup-btn" onClick={() => setSignUp(!signUp)}>Sign up</button>
       </div>
