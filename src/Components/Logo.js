bb1761618f8af474a319ad3a5ad67b2c0be0359e
t-hoffman <tjhoffmn@gmail.com>
2024-11-05 20:06:27 -0800

    Cleaned up code and made the fade in/out of the navbar smoother and more efficient with less re-renders.

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-export default function Logo({ width = 30, height = 30 }) {
-  return (
-    <img
-      src="/xlogo.svg"
-      id="x-logo"
-      width={`${width}px`}
-      height={`${height}px`}
-    />
-  );
-}
+export default ({ width = 30, height = 30 }) => (
+  <img
+    src="/xlogo.svg"
+    id="x-logo"
+    width={`${width}px`}
+    height={`${height}px`}
+  />
+);
